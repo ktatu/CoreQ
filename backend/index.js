1716b1920b5d50437c4425a6a54e192228e5a31b
ktatu <kaikkonentatu@gmail.com>
2021-09-24 21:26:17 +0300

    Tiedostojen uploadaus frontista backiin

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,27 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 
 const app = express()
 
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 
 app.use(cors())
+app.use(fileUpload())
 app.use(express.json())
 app.use(morgan(':method :url :status :body'))
 
 app.get('/', (req, res) => {
-    let test = {"hello": "world"}
+    let test = {'hello': 'world'}
     res.json(test)
+})
+
+app.post('/api/file', (req, res) => {
+    console.log('ollaan file uploadin controllerissa')
+    // uploadyssa default inputFieldName on "file"
+    let file = req.files.file
+    file ? res.status(200) : res.status(404)
 })
 
 const PORT = 3001
--- frontend/coreq/src/App.js
+++ frontend/coreq/src/App.js
@@ -1,21 +1,21 @@
 import testService from './services/test'
 import React, {useState} from 'react'
 
+import FileUpload from './components/FileUpload'
+
 const App = () => {
     const [testState, setTestState] = useState('')
 
-    testService.get().then((test) => {
-        console.log("testprint", test.hello)    
-    })
-
     // tarvitaan .then() promise valuen saamiseksi, muuten palauttaa promisen
-    testService.asynch().then(data => setTestState(data.hello))
+    //testService.asynch().then(data => setTestState(data.hello))
 
     return (
         <div className="App">
-        <p>Terve {testState}</p>
+            <p>{testState}</p>
+            <FileUpload/>
         </div>
+
     );
 }
 
-export default App;
+export default App
