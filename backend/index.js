1700aba5da04ccf206ab6337ab951553e480c1b7
ktatu <kaikkonentatu@gmail.com>
2021-09-25 15:28:33 +0300

    File upload-konfigurointia, error handling sentryllä

--- backend/index.js
+++ backend/index.js
@@ -3,20 +3,30 @@ const morgan = require('morgan')
 const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const fileUploadRouter = require('./controllers/file_upload')
+const sentry = require('@sentry/node')
+const config = require('./utils/config')
 
 const app = express()
 
+sentry.init({
+    dsn: config.SENTRY_KEY,
+    integrations: [
+      new sentry.Integrations.Http({ tracing: true }),
+    ]
+  })
+
 morgan.token('body', function (req) { return JSON.stringify(req.body) })
 
+app.use(Sentry.Handlers.requestHandler())
 app.use(cors())
 app.use(fileUpload({
+    abortOnLimit: true,
     limits: { fileSize: 1000000 },
-    useTempFiles: true,
-    abortOnLimit: true
+    safeFileNames: true,
+    useTempFiles: true
 }))
 app.use(express.json())
 app.use(morgan(':method :url :status :body'))
-
 app.use('/api/file', fileUploadRouter)
 
 app.get('/', (req, res) => {
@@ -24,19 +34,11 @@ app.get('/', (req, res) => {
     res.json(test)
 })
 
-/*
-app.post('/api/file', (req, res) => {
-    console.log('ollaan file uploadin controllerissa')
-    if (!req.files) {
-        return res.status(400).send('No files were uploaded')
-    }
-    // uploady's default inputFieldName is "file"
-    let file = req.files.file
-    console.log("file mimetype "+ file.mimetype)
-    
-    res.status(200).send('File uploaded')
+app.get("/debug-sentry", (req, res) => {
+    throw new Error("My nth Sentry error!");
 })
-*/
+
+app.use(sentry.Handlers.errorHandler())
 
 const PORT = 3001
 app.listen(PORT, () => {
---
+++ backend/utils/config.js
@@ -0,0 +1,5 @@
+require('dotenv').config()
+
+const SENTRY_KEY = process.env.SENTRY_KEY
+
+export { SENTRY_KEY }
