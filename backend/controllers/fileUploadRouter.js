e1e45cc72663085e511e64f10768ded69a565422
ktatu <kaikkonentatu@gmail.com>
2021-10-21 14:12:20 +0300

    Set restrictions on uploaded file extensions

--- backend/controllers/fileUploadRouter.js
+++ backend/controllers/fileUploadRouter.js
@@ -1,13 +1,18 @@
 const fileUploadRouter = require('express').Router()
+const path = require('path')
+
+const allowedExtensions = ['.java', '.js', '.py']
 
 fileUploadRouter.post('/', (req, res) => {
-    console.log('ollaan file uploadin controllerissa')
     if (!req.files) {
         return res.status(400).send('No files were uploaded')
     }
     // uploady's default inputFieldName is "file"
     let file = req.files.file
-    console.log("file mimetype "+ file.mimetype)
+    const ext = path.extname(file.name)
+    if (!allowedExtensions.includes(ext)) {
+        return res.status(400).send('Extension type ' + ext + ' not allowed')
+    }
     
     res.status(200).send('File uploaded')
 })
